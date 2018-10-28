16fc39e9994c0cdbb20fc8ded4f4eb2cf0ef3495
Vanessa Asse <vanessa.asse@gmail.com>
2018-10-28 19:43:49 +0100

    Création du fichier d'envoi du mail + message de succès d'envoi et d'erreur

---
+++ sendMail/errorMail.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <title>Demande de devis - Erreur lors de l'envoi de l'email</title>
+
+</head>
+
+<body>
+
+<H1>Commencez votre projet : demandez un devis</H1>
+
+<div>
+    <p>&nbsp;</p>
+    <p>Une erreur est survenue lors de l'enregistrement de votre demande.</p>
+    <p>Veuillez la saisir à nouveau.</p> <!-- TODO Ajouter le lien vers le formulaire -->
+</div>
---
+++ sendMail/mailOk.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <title>Demande de devis - Message envoyé</title>
+
+</head>
+
+<body>
+
+<H1>Commencez votre projet : demandez un devis</H1>
+
+<div>
+    <p>&nbsp;</p>
+    <p>Votre demande de devis a bien été envoyée !</p>
+    <p>Nous allons revenir vers vous très rapidement.</p>
+</div>
