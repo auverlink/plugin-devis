160c8e083532f5a383f849c6d95acc26b6077970
Vanessa Asse <vanessa.asse@gmail.com>
2018-11-03 12:01:35 +0100

    Mise en commentaires de la fonction getPluginDevis

--- plugin-devis.php
+++ plugin-devis.php
@@ -12,15 +12,7 @@ Version: 1.0
 */
 
 /*add_action( 'template_redirect', 'getPluginDevis' );
-
-function getPluginDevis()
-{
-    $url = '/plugin-devis/index.php';
-
-    if(is_page('devis'))
-    {
-        wp_redirect($url);
-        die;
-    }
 }*/
 
+
+?>
